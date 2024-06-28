d7bfa02ea1e5ff3ff342a9e1b53f3ef9e84f45d1
jpphang <joyanna_phang1@my.vcccd.edu>
2024-06-27 21:35:22 -0700

    4-2

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def main():
     """
     total = 0
     
-    for i in range(5)
+    for i in range(5):
+        user_input=int(input(f'enter numer{i+1}:'))
+        
+        total += user_input
+        
 
     ########################################
     # Do not delete the return statement
